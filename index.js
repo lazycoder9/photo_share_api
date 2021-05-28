abb06c3b14e915fec1185a59a64524c90855eaf9
Ulugbek Tuychiev <t.ulugbek.9@gmail.com>
2021-05-28 16:50:23 +0500

    add User type, create o2m relation between photo and user, mock data

--- index.js
+++ index.js
@@ -1,6 +1,13 @@
 const { ApolloServer } = require('apollo-server');
 
 const typeDefs = `
+  type User {
+    githubLogin: ID!
+    name: String
+    avatar: String
+    postedPhotos: [Photo!]!
+  }
+
   enum PhotoCategory {
     SELFIE
     PORTRAIT
@@ -15,6 +22,7 @@ const typeDefs = `
     name: String!
     description: String
     category: PhotoCategory!
+    postedBy: User!
   }
 
   input PostPhotoInput {
@@ -34,7 +42,34 @@ const typeDefs = `
 `;
 
 let _id = 0;
-const photos = [];
+const photos = [
+  {
+    id: '1',
+    name: 'Dropping the Heart Chute',
+    description: 'The heart chute is one of my favorite chutes',
+    category: 'ACTION',
+    githubUser: 'gPlake',
+  },
+  {
+    id: '2',
+    name: 'Enjoying the sunshine',
+    category: 'SELFIE',
+    githubUser: 'sSchmidt',
+  },
+  {
+    id: '3',
+    name: 'Gunbarrel 25',
+    description: '25 laps on gunbarrel today',
+    category: 'LANDSCAPE',
+    githubUser: 'sSchmidt',
+  },
+];
+
+const users = [
+  { githubLogin: 'mHattrup', name: 'Mike Hattrup' },
+  { githubLogin: 'gPlake', name: 'Glen Plake' },
+  { githubLogin: 'sSchmidt', name: 'Scot Schmidt' },
+];
 
 const resolvers = {
   Query: {
@@ -53,6 +88,10 @@ const resolvers = {
   },
   Photo: {
     url: (parent) => `http://myphotos.com/photo/${parent.id}.jpg`,
+    postedBy: (parent) => users.find((u) => parent.githubUser === u.githubLogin),
+  },
+  User: {
+    postedPhotos: (parent) => photos.filter((p) => p.githubUser === parent.githubLogin),
   },
 };
 
