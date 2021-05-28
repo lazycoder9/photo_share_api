066bfff7dc9bdc678351b46569dad913f747825b
Ulugbek Tuychiev <t.ulugbek.9@gmail.com>
2021-05-28 16:27:23 +0500

    add Photo type, enchance postPhoto mutation, add allPhotos query

--- index.js
+++ index.js
@@ -1,26 +1,43 @@
 const { ApolloServer } = require('apollo-server');
 
 const typeDefs = `
+  type Photo {
+    id: ID!
+    url: String!
+    name: String!
+    description: String
+  }
+
   type Query {
     totalPhotos: Int!
+    allPhotos: [Photo!]!
   }
 
   type Mutation {
-    postPhoto(name: String! description: String): Boolean!
+    postPhoto(name: String! description: String): Photo!
   }
 `;
 
+let _id = 0;
 const photos = [];
 
 const resolvers = {
   Query: {
     totalPhotos: () => photos.length,
+    allPhotos: () => photos,
   },
   Mutation: {
     postPhoto(_parent, args) {
-      photos.push(args);
-      return true;
+      const newPhoto = {
+        id: _id++,
+        ...args,
+      };
+      photos.push(newPhoto);
+      return newPhoto;
     },
+  },
+  Photo: {
+    url: (parent) => `http://myphotos.com/photo/${parent.id}.jpg`,
   },
 };
 
