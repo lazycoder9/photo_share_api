39d5440e80b6364a863e1ed9484c6467008b31b5
Ulugbek Tuychiev <t.ulugbek.9@gmail.com>
2021-05-28 17:00:31 +0500

    add tags, taggedUsers and inPhotos fields

--- index.js
+++ index.js
@@ -6,6 +6,7 @@ const typeDefs = `
     name: String
     avatar: String
     postedPhotos: [Photo!]!
+    inPhotos: [Photo!]!
   }
 
   enum PhotoCategory {
@@ -23,6 +24,7 @@ const typeDefs = `
     description: String
     category: PhotoCategory!
     postedBy: User!
+    taggedUsers: [User!]!
   }
 
   input PostPhotoInput {
@@ -70,6 +72,12 @@ const users = [
   { githubLogin: 'gPlake', name: 'Glen Plake' },
   { githubLogin: 'sSchmidt', name: 'Scot Schmidt' },
 ];
+const tags = [
+  { photoID: '1', userID: 'gPlake' },
+  { photoID: '2', userID: 'sSchmidt' },
+  { photoID: '2', userID: 'mHattrup' },
+  { photoID: '2', userID: 'gPlake' },
+];
 
 const resolvers = {
   Query: {
@@ -89,9 +97,19 @@ const resolvers = {
   Photo: {
     url: (parent) => `http://myphotos.com/photo/${parent.id}.jpg`,
     postedBy: (parent) => users.find((u) => parent.githubUser === u.githubLogin),
+    taggedUsers: (parent) =>
+      tags
+        .filter((tag) => tag.photoID === parent.id)
+        .map((tag) => tag.userID)
+        .map((userID) => users.find((u) => u.githubLogin === userID)),
   },
   User: {
     postedPhotos: (parent) => photos.filter((p) => p.githubUser === parent.githubLogin),
+    inPhotos: (parent) =>
+      tags
+        .filter((tag) => tag.userID === parent.id)
+        .map((tag) => tag.photoID)
+        .map((photoID) => photos.find((p) => p.id === photoID)),
   },
 };
 
