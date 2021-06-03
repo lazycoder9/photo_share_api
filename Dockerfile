0a3121067858274054c44125be72d3d9f864ddb2
Ulugbek Tuychiev <t.ulugbek.9@gmail.com>
2021-06-04 02:24:46 +0500

    configure mongo in docker-compose, move rom stub data to mongo requests

--- Dockerfile
+++ Dockerfile
@@ -1,4 +1,4 @@
-FROM node:14-buster-slim
+FROM node:14
 
 RUN apt update && apt -y upgrade
 
--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+const { MongoClient } = require('mongodb');
 const { ApolloServer } = require('apollo-server-express');
 const express = require('express');
 const expressPlayground = require('graphql-playground-middleware-express').default;
@@ -5,55 +6,19 @@ const { readFileSync } = require('fs');
 const typeDefs = readFileSync('./typeDefs.graphql', 'UTF-8');
 const resolvers = require('./resolvers');
 
-let _id = 0;
-const photos = [
-  {
-    id: '1',
-    name: 'Dropping the Heart Chute',
-    description: 'The heart chute is one of my favorite chutes',
-    category: 'ACTION',
-    githubUser: 'gPlake',
-    created: '3-28-1977',
-  },
-  {
-    id: '2',
-    name: 'Enjoying the sunshine',
-    category: 'SELFIE',
-    githubUser: 'sSchmidt',
-    created: '1-2-1985',
-  },
-  {
-    id: '3',
-    name: 'Gunbarrel 25',
-    description: '25 laps on gunbarrel today',
-    category: 'LANDSCAPE',
-    githubUser: 'sSchmidt',
-    created: '2018-04-15T19:09:57.308Z',
-  },
-];
+require('dotenv').config();
 
-const users = [
-  { githubLogin: 'mHattrup', name: 'Mike Hattrup' },
-  { githubLogin: 'gPlake', name: 'Glen Plake' },
-  { githubLogin: 'sSchmidt', name: 'Scot Schmidt' },
-];
-const tags = [
-  { photoID: '1', userID: 'gPlake' },
-  { photoID: '2', userID: 'sSchmidt' },
-  { photoID: '2', userID: 'mHattrup' },
-  { photoID: '2', userID: 'gPlake' },
-];
+async function start() {
+  const app = express();
+  const MONGO_DB = process.env.DB_HOST;
+  const client = await MongoClient.connect(MONGO_DB, { useNewUrlParser: true });
+  const db = client.db();
+  const context = { db };
+  const server = new ApolloServer({ typeDefs, resolvers, context });
+  server.applyMiddleware({ app });
+  app.get('/', (req, res) => res.end('Welcome to the PhotoShare API'));
+  app.get('/playground', expressPlayground({ endpoint: '/graphql' }));
+  app.listen({ port: 4000 }, () => console.log(`GraphQL Server running at http://localhost:4000${server.graphqlPath}`));
+}
 
-const app = express();
-
-const server = new ApolloServer({
-  typeDefs,
-  resolvers,
-});
-
-server.applyMiddleware({ app });
-
-app.get('/', (req, res) => res.end('Welcome to the PhotoShareAPI'));
-app.get('/playground', expressPlayground({ endpoint: '/graphql' }));
-
-app.listen({ port: 4000 }, () => console.log(`GraphQL Server running @ http://localhost:4000${server.graphqlPath}`));
+start();
