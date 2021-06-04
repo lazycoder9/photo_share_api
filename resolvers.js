50f0050356c2f4b684b53fa2c4cdfbadf0c1012d
Ulugbek Tuychiev <t.ulugbek.9@gmail.com>
2021-06-04 21:00:17 +0500

    implement github authorization, ignore .env

--- resolvers.js
+++ resolvers.js
@@ -1,4 +1,5 @@
 const { GraphQLScalarType } = require('graphql');
+const { authorizeWithGithub } = require('./lib.js');
 
 module.exports = {
   Query: {
@@ -16,6 +17,30 @@ module.exports = {
       };
       photos.push(newPhoto);
       return newPhoto;
+    },
+    async githubAuth(_parent, { code }, { db }) {
+      const { message, access_token, avatar_url, login, name } = await authorizeWithGithub({
+        client_id: process.env.CLIENT_ID,
+        client_secret: process.env.CLIENT_SECRET,
+        code,
+      });
+
+      if (message) {
+        throw new Error(message);
+      }
+
+      const latestUserInfo = {
+        name,
+        githubLogin: login,
+        githubToken: access_token,
+        avatar: avatar_url,
+      };
+
+      const {
+        ops: [user],
+      } = await db.collection('users').replaceOne({ githubLogin: login }, latestUserInfo, { upsert: true });
+
+      return { user, token: access_token };
     },
   },
   Photo: {
